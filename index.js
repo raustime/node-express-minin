cdc1265384907085290fa4b67ac044e12d4dd126
poweruser <C:\Users\1\AppData\Roaming\The Bat!>
2020-10-16 17:12:08 +0300

    first step on project done

---
+++ index.js
@@ -0,0 +1,35 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const exphbs=require('express-handlebars');
+const todoRoutes=require('./routes/todos');
+
+
+const PORT=process.env.PORT || 3000;
+
+const app=express();
+const hbs=exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views','views');
+
+app.use(todoRoutes);
+
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://testUser1:dFgvB123_fgDr@cluster0.pwjuy.mongodb.net/todos',{
+            useNewUrlParser: true,
+            useFindAndModify: false
+        });
+        app.listen(PORT, ()=>{
+            console.log(`server started at port ${PORT}`);
+        });
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+start();
